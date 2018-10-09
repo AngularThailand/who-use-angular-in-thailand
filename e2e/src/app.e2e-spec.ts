a603425842cbc9b81b9bc8a367ccf92bf91dfc9a
PerJerZ <perjerz3434@gmail.com>
2018-10-10 01:50:24 +0700

    fixed e2e h1

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to who-use-angular-in-thailand!');
+    expect(page.getParagraphText()).toEqual('Who use angular in Thailand');
   });
 });
