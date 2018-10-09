699af54bfd967b012512d465bba4be1d7ac7671c
PerJerZ <perjerz3434@gmail.com>
2018-10-10 02:07:15 +0700

    removed debugging path

--- deploy.sh
+++ deploy.sh
@@ -3,19 +3,15 @@ if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
   git config --global user.email $GH_EMAIL > /dev/null 2>&1
   git clone $CIRCLE_REPOSITORY_URL project
   cd project
-  ls
   git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
   git rm -rf .
   cd ..
-  ls
+
 
   cp -a dist/who-use-angular-in-thailand/. project/.
 
   mkdir -p project/.circleci && cp -a .circleci/. project/.circleci/.
-  ls
-  pwd
   cd project
-  ls
 
   git add -A
   git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
