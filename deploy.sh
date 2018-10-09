0edd346c542fde94c15deec20b51e25a528f7b97
PerJerZ <perjerz3434@gmail.com>
2018-10-10 02:00:42 +0700

    debug deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,23 @@
 if [ $CIRCLE_BRANCH == $SOURCE_BRANCH ]; then
   git config --global user.name $GH_NAME > /dev/null 2>&1
   git config --global user.email $GH_EMAIL > /dev/null 2>&1
+  pwd
+  ls
   git clone $CIRCLE_REPOSITORY_URL project
   cd project
+  ls
   git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
   git rm -rf .
   cd ..
+  ls
 
   cp -a dist/. project/.
 
   mkdir -p project/.circleci && cp -a .circleci/. project/.circleci/.
-
+  ls
+  pwd
   cd project
+  ls
 
   git add -A
   git commit -m "Automated deployment to GitHub Pages: ${CIRCLE_SHA1}" --allow-empty
