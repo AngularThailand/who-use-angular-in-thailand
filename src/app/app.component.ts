52773c9af61d8849fc491e9433cc848c0c03e068
PerJerZ <perjerz3434@gmail.com>
2018-10-13 04:26:34 +0700

    feature: company card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Company } from './company.model';
+import { map, share } from 'rxjs/operators';
 
 @Component({
   selector: 'angular-th-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'who-use-angular-in-thailand';
+export class AppComponent implements OnInit {
+  companies$: Observable<Company[]>;
+  constructor(private http: HttpClient) { }
+  ngOnInit() {
+    this.companies$ = this.http.get<{companies: Company[]}>('/assets/data/companies.json').pipe(map( ({companies}) => companies), share());
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,40 @@
-import { BrowserModule } from '@angular/platform-browser';
+
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
-
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { CustomMaterialModule } from './shared/custom-material.module';
+import { CompanyCardComponent } from './company-card/company-card.component';
+
+import { TechToIconPipe } from './company-card/tech-to-icon.pipe';
+
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CompanyListComponent } from './company-list/company-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CompanyCardComponent,
+    TechToIconPipe,
+    FooterComponent,
+    CompanyListComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
