23df8fbe6a0597a35e14a56a6921aa4c9e503008
PerJerZ <perjerz3434@gmail.com>
2018-10-16 02:10:10 +0700

    fixed: fixing company sevice (destruct companies key)

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { CompanyService } from './services/company.service';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import { HttpClient } from '@angular/common/http';
 import { mockCompany } from './utils/mock-company';
 import { of } from 'rxjs';
 
--- src/app/services/company.service.spec.ts
+++ src/app/services/company.service.spec.ts
@@ -1,12 +1,16 @@
-import { TestBed } from '@angular/core/testing';
-
+import { async } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { mockCompany } from './../utils/mock-company';
 import { CompanyService } from './company.service';
 
-xdescribe('CompanyService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
-    const service: CompanyService = TestBed.get(CompanyService);
-    expect(service).toBeTruthy();
-  });
+describe('CompanyService', () => {
+  it('should destruct companies key into company list', async(() => {
+    const companies = [mockCompany()];
+    const httpMock = { get: jest.fn(() => of({companies})) };
+    const service = new CompanyService(httpMock as any);
+    service.getCompanies().subscribe(data => {
+      expect(httpMock.get).toHaveBeenCalledWith('/assets/data/companies.json');
+      expect(data).toBe(companies);
+    });
+  }));
 });
