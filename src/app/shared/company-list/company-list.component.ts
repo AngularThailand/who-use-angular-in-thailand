bc93021891ea76be8c3151f2e92f206ab6aa2129
PerJerZ <perjerz3434@gmail.com>
2018-10-24 21:11:07 +0700

    remove: removed double import @angular/material/theming

--- src/app/shared/company-list/company-list.component.ts
+++ src/app/shared/company-list/company-list.component.ts
@@ -9,7 +9,7 @@ import { Component, Input, ChangeDetectionStrategy } from '@angular/core';
 })
 export class CompanyListComponent  {
   @Input() companies: Company[];
-  @Input() loaded;
+  @Input() loaded: boolean;
   constructor() { }
 
 }
