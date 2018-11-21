c1d14c655ced7f930a474a0cb4b93f1483431c21
Ittipol Thirasat <hisoft_mix@hotmail.com>
2018-11-21 21:03:12 +0700

    add: nodejs pwa scss webrtc

--- src/app/shared/company-card/technologies.ts
+++ src/app/shared/company-card/technologies.ts
@@ -12,11 +12,21 @@ export const Technologies = {
   nest: 'nest',
   nestjs: 'nest',
   ngrx: 'ngrx',
+  // https://nodejs.org/en/about/resources/
+  node: 'nodejs',
+  nodejs: 'nodejs',
   nativescript: 'nativescript',
+  // https://github.com/webmaxru/progressive-web-apps-logo -> https://diekus.com/logo-pwinter/
+  pwa: 'pwa',
   python: 'python',
   rx: 'rxjs',
   rxjs: 'rxjs',
+  // https://github.com/sass/sass-site/blob/master/source/assets/img/logos/logo.svg
+  sass: 'sass',
+  scss: 'sass',
   ts: 'typescript',
   tensorflow: 'tensorflow',
   typescript: 'typescript',
+  // https://github.com/webrtc/webrtc-org -> https://github.com/webrtc/webrtc-org/blob/gh-pages/assets/images/webrtc-logo-vert.svg
+  webrtc: 'webrtc',
 };
