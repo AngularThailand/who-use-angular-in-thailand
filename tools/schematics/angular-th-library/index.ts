941877dfd05573f3f2e6ede925925e87e66912c4
perjerz3434 <perjerz3434@gmail.com>
2019-06-23 17:28:36 +0700

    add workspace-schematic name angular-th-library

---
+++ tools/schematics/angular-th-library/index.ts
@@ -0,0 +1,48 @@
+import { chain, externalSchematic, Rule } from '@angular-devkit/schematics';
+
+export default function(schema: any): Rule {
+  const { name } = schema;
+  const style = 'scss';
+  return chain([
+    externalSchematic(
+      '@nrwl/schematics',
+      'lib',
+      {
+        name: `${name}/components`,
+        style,
+        tags: `scope:${name},type:component`
+      },
+      { interactive: false }
+    ),
+    externalSchematic(
+      '@nrwl/schematics',
+      'lib',
+      {
+        name: `${name}/http`,
+        style,
+        tags: `scope:${name},type:http`
+      },
+      { interactive: false }
+    ),
+    externalSchematic(
+      '@nrwl/schematics',
+      'lib',
+      {
+        name: `${name}/pages`,
+        style,
+        tags: `scope:${name},type:pages`
+      },
+      { interactive: false }
+    ),
+    externalSchematic(
+      '@nrwl/schematics',
+      'lib',
+      {
+        name: `${name}/resolvers`,
+        style,
+        tags: `scope:${name},type:resolvers`
+      },
+      { interactive: false }
+    )
+  ]);
+}
