f86d42349769f6231e1247cbc6d26106b3a19d2f
perjerz3434 <perjerz3434@gmail.com>
2019-06-25 00:16:29 +0700

    remove angular workspace files

--- projects/angular-th/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of angular-th
- */
-
-export * from './lib/header/header.component';
-export * from './lib/footer/footer.component';
-export * from './lib/angular-th-libs.module';
