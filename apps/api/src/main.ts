fba9993b2a9385efe37fe04041854e144d1b7baa
perjerz3434 <perjerz3434@gmail.com>
2019-06-24 20:08:23 +0700

    fix cors

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -10,6 +10,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.enableCors({ origin: 'http://localhost:4200' });
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.port || 3333;
   await app.listen(port, () => {
