0269fe07edc26b11e6b89ea12c1be795b309cfe0
PerJerZ <siwat.kaolueng@flowaccount.com>
2020-11-17 00:04:28 +0700

    fix tsconfig, remove jest

--- apps/who-use-angular-in-thailand/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  name: 'who-use-angular-in-thailand',
-  preset: '../../jest.config.js',
-  coverageDirectory: '../../coverage/apps/who-use-angular-in-thailand',
-  snapshotSerializers: [
-    'jest-preset-angular/AngularSnapshotSerializer.js',
-    'jest-preset-angular/HTMLCommentSerializer.js',
-  ],
-};
